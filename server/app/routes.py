41974a7f8a06e602bb7020b81aa3ebe9530f3f4b
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-09-17 19:22:34 -0400

    Tracks Routes

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,11 @@
-from views import (
-    test_view,
-    get_artists_view, post_artists_view, put_artists_view, delete_artists_view
-)
+from views.artists_views import get_artists_view, post_artists_view, put_artists_view, delete_artists_view
+from views.tracks_views import get_track_view, post_tracks_view, put_tracks_view, delete_tracks_view
+
+
+async def test_view(request):
+    return web.json_response({
+        "message": "Yo mamma so fat even penguins are jealous of the way she waddles."
+    })
 
 
 def setup_routes(app):
@@ -11,3 +15,8 @@ def setup_routes(app):
     app.router.add_post("/api/artists", post_artists_view)
     app.router.add_put("/api/artists", put_artists_view)
     app.router.add_delete("/api/artists/{id}", delete_artists_view)
+
+    app.router.add_get("/api/tracks/{id}", get_track_view)
+    app.router.add_post("/api/tracks", post_tracks_view)
+    app.router.add_put("/api/tracks", put_tracks_view)
+    app.router.add_delete("/api/tracks/{id}", delete_tracks_view)
--- server/app/views/artists_views.py
+++ server/app/views/artists_views.py
@@ -3,12 +3,6 @@ from aiohttp import web
 import db
 
 
-async def test_view(request):
-    return web.json_response({
-        "message": "Yo mamma so fat even penguins are jealous of the way she waddles."
-    })
-
-
 async def get_artists_view(request):
     async with request.app["db"].acquire() as conn:
         cursor = await conn.execute(db.artists.select())
@@ -29,7 +23,7 @@ async def post_artists_view(request):
         )
         artist = await result.fetchone()
         return web.json_response({
-            "id": artist[0],
+            "id": artist["id"],
             "message": "Artist created successfully"
         })
 
