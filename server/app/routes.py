107e73e79d812806a3122028ee1ee9ecd783d4b2
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-09-17 16:14:45 -0400

    PUT /api/artists route

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,12 @@
-from views import test_view, get_artists_view, post_artists_view
+from views import (
+    test_view,
+    get_artists_view, post_artists_view, put_artists_view
+)
 
 
 def setup_routes(app):
     app.router.add_get("/api/test", test_view)
+
     app.router.add_get("/api/artists", get_artists_view)
     app.router.add_post("/api/artists", post_artists_view)
+    app.router.add_put("/api/artists", put_artists_view)
--- server/app/views.py
+++ server/app/views.py
@@ -32,3 +32,24 @@ async def post_artists_view(request):
             "id": artist[0],
             "message": "Artist created successfully"
         })
+
+
+async def put_artists_view(request):
+    async with request.app["db"].acquire() as conn:
+        data = await request.json()
+        result = await conn.execute(
+            db.artists.update()
+            .returning(*db.artists.c)
+            .where(db.artists.c.id == data["id"])
+            .values(
+                name=data["name"],
+                websiteUrl=data.get("websiteUrl"),
+                location=data.get("location")
+            )
+        )
+        artist = await result.fetchone()
+        if not artist:
+            raise web.HTTPNotFound(text="Artist with id {} does not exist".format(data["id"]))
+        return web.json_response({
+            "message": "Artist updated successfully"
+        })
